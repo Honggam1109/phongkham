2890bb6520a8582d88684326b22363b240db4b83
cuongtring25 <cuongtrinh574@gmail.com>
2025-04-03 16:54:06 +0700

    to chuc lai file code

---
+++ main.py
@@ -0,0 +1,14 @@
+#import library
+from flask import Flask,render_template
+from routes import  main_bp
+import pyodbc
+import sqlite3
+app = Flask(__name__,static_url_path='/static')
+
+# Đăng ký Blueprint
+app.register_blueprint(main_bp)
+
+
+#MAIN RUN
+if __name__ == '__main__':
+    app.run()
---
+++ routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint,render_template
+
+main_bp = Blueprint('main', __name__)
+
+# routes cua benh nhan
+@main_bp.route('/')
+def homepage():
+    return render_template('homepage.html')
+@main_bp.route('/bacsi')
+def bacsi():
+    return render_template('bacsi.html')
+@main_bp.route('/blog')
+def blog():
+    return render_template('blog.html')
+@main_bp.route('/datlich')
+def datlich():
+    return render_template('datlich.html')
+@main_bp.route('/dichvu')
+def dichvu():
+    return render_template('dichvu.html')
+@main_bp.route('/vechungtoi')
+def vechungtoi():
+    return render_template('vechungtoi.html')
+@main_bp.route('/lamsachrang')
+def lamsachrang():
+    return render_template('lamsachrang.html')
+#routes cua bac si
+# @main_bp.route('/')
+# def home():
+#     return render_template('home.html')
+@main_bp.route('/benhnhan')
+def benhnhan():
+    return render_template('benhnhan.html')
